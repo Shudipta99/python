f30ed7f58359ef7c5924138e3df732b7a4d87e3a
shudipta banik <shudiptobanik99@gmail.com>
2022-01-14 01:33:58 +0600

    Second commit

--- If_else_elif.py
+++ If_else_elif.py
@@ -2,6 +2,6 @@ num1 = int(input("Enter your Num1: "))
 if(num1>=50):
     print("Num1 is medium")
 elif(num1>=80):
-    print("Num2 is High")    
+    print("Num2 is very High")    
 else:
     print("Num2 is very low")
